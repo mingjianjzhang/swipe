9f3aff7b0a889b0fb8a241dfdef2dc99513d084f
Josh Zhang <josh.zhang91@gmail.com>
2017-09-26 15:51:05 -0700

    initialized PanResponder

--- src/Deck.js
+++ src/Deck.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Animated, Text, Image } from 'react-native';
+import { StyleSheet, View, Animated, Text, Image, PanResponder } from 'react-native';
 
 class Deck extends Component {
+	constructor(props) {
+		super(props);
+		
+		const panResponder = PanResponder.create({
+			onStartShouldSetPanResponder: () => {},
+			onPanResponderMove: () => {},
+			onPanResponderRelease: () => {}
+		});
 
-	componentWillMount(){
-		console.log(this.props, "my deck props");
+		this.state = { panResponder };
+	
 	}
+	
+	componentWillMount(){
 
+	}
+	
 	renderCards(){
 		return this.props.data.map( item => {
 			return this.props.renderCard(item)
-		})
+		});
 	}
 	render(){
 		return (
